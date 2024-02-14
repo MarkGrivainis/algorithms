e589450f68ff63a4e4239d5bff2e5c228aecbbf8
Mark Grivainis <markgrivainis@gmail.com>
2024-02-14 08:47:47 -0500

    feat: add linked list builtins
    + add len logic to linked list
    + add iter to iterate over the list
    + clean up docstrings
    + clean up unit tests

--- src/algorithms/data_structures/linked_list.py
+++ src/algorithms/data_structures/linked_list.py
@@ -1,6 +1,6 @@
 """Implementation of a linked list."""
 from dataclasses import dataclass
-from typing import Generic, Optional, TypeVar
+from typing import Generic, Iterator, Optional, TypeVar
 
 T = TypeVar("T")
 
@@ -10,11 +10,12 @@ class Node(Generic[T]):
     """Node of a linked list containing a value and a pointer to the next node."""
 
     value: T
-    next: Optional["Node[T]"]
+    next: Optional["Node[T]"] = None
 
 
 class LinkedList(Generic[T]):
-    """Linked list data structure.
+    """
+    Implementation of a Linked list data structure.
 
     :param T: The type of the value stored in the linked list.
     :type T: TypeVar
@@ -40,7 +41,8 @@ class LinkedList(Generic[T]):
         self.head: Optional[Node[T]] = None
 
     def search(self, value: T) -> Optional[T]:
-        """Search for a value in the linked list.
+        """
+        Search for a value in the linked list.
 
         :param value: Value to search for.
         :type value: T
@@ -57,7 +59,8 @@ class LinkedList(Generic[T]):
             current = current.next
 
     def insert(self, value: T) -> None:
-        """Insert a value at the front of the linked list.
+        """
+        Insert a value at the front of the linked list.
 
         :param value: Value to insert.
         :type value: T
@@ -75,7 +78,8 @@ class LinkedList(Generic[T]):
         self.head = Node[T](value, self.head)
 
     def delete(self, value: T) -> None:
-        """Delete a value from the linked list.
+        """
+        Delete a value from the linked list.
 
         :param value: Value to delete.
         :type value: T
@@ -93,16 +97,25 @@ class LinkedList(Generic[T]):
             >>> linked_list
             4 -> 2 -> 1 -> None
         """
+        if self.search(value) is None:
+            return
+
         current: Optional[Node[T]] = self.head
+        if current is not None and current.value == value:
+            self.head = current.next
+            return
+
         while True:
             if current is None:
-                break
+                return
             if current.next is not None and current.next.value == value:
                 current.next = current.next.next
+                return
             current = current.next
 
     def __str__(self) -> str:
-        """Return a string representation of the linked list.
+        """
+        Return a string representation of the linked list.
 
         :return: String representation of the linked list.
         :rtype: str
@@ -119,9 +132,61 @@ class LinkedList(Generic[T]):
         return result
 
     def __repr__(self) -> str:
-        """Return a string representation of the linked list.
+        """
+        Return a string representation of the linked list.
 
         :return: String representation of the linked list.
         :rtype: str
         """
         return str(self)
+
+    def __len__(self) -> int:
+        """
+        Return the length of the linked list.
+
+        :return: Length of the linked list.
+        :rtype: int
+
+        :Example:
+            >>> from algorithms.data_structures.linked_list import LinkedList
+            >>> linked_list = LinkedList()
+            >>> linked_list.insert(1)
+            >>> linked_list.insert(2)
+            >>> linked_list.insert(3)
+            >>> linked_list.insert(4)
+            >>> len(linked_list)
+            4
+        """
+        current: Optional[Node[T]] = self.head
+        length = 0
+        while True:
+            if current is None:
+                break
+            length += 1
+            current = current.next
+        return length
+
+    def __iter__(self) -> Iterator[T]:
+        """
+        Return an iterator over the linked list.
+
+        :return: Iterator over the linked list.
+        :rtype: Iterator[T]
+
+        :Example:
+            >>> from algorithms.data_structures.linked_list import LinkedList
+            >>> linked_list = LinkedList()
+            >>> linked_list.insert(1)
+            >>> linked_list.insert(2)
+            >>> linked_list.insert(3)
+            >>> linked_list.insert(4)
+            >>> list(linked_list)
+            [4, 3, 2, 1]
+
+        """
+        current: Optional[Node[T]] = self.head
+        while True:
+            if current is None:
+                break
+            yield current.value
+            current = current.next
--- tests/data_structures/test_linked_lists.py
+++ tests/data_structures/test_linked_lists.py
@@ -13,39 +13,79 @@ class TestLinkedList:
     def test_linked_list_search_found(self) -> None:
         """Test linked list search for an existing value."""
         ll = LinkedList[int]()
-        ll.insert(2)
-        assert ll.search(2) == 2
+        ll.insert(1)
+        assert ll.search(1) == 1
 
     def test_linked_list_search_not_found(self) -> None:
         """Test linked list search for a non-existing value."""
         ll = LinkedList[int]()
-        ll.insert(2)
-        assert ll.search(3) is None
+        ll.insert(1)
+        assert ll.search(2) is None
 
     def test_linked_list_insert(self) -> None:
         """Test linked list insert."""
         ll = LinkedList[int]()
-        ll.insert(2)
-        assert ll.search(2) == 2
+        ll.insert(1)
+        assert str(ll) == "1 -> None"
+        assert ll.search(1) == 1
 
     def test_linked_list_delete(self) -> None:
         """Test linked list delete."""
         ll = LinkedList[int]()
+        ll.insert(1)
         ll.insert(2)
         ll.insert(3)
         ll.delete(2)
+        assert str(ll) == "3 -> 1 -> None"
         assert ll.search(2) is None
 
     def test_linked_list_delete_not_found(self) -> None:
         """Test linked list delete for a non-existing value."""
         ll = LinkedList[int]()
+        ll.insert(1)
         ll.insert(2)
-        ll.insert(3)
-        ll.delete(4)
-        assert ll.search(4) is None
+        ll.delete(3)
+        assert ll.search(3) is None
+        assert str(ll) == "2 -> 1 -> None"
+
+    def test_linked_list_delete_empty(self) -> None:
+        """Test linked list delete for an empty list."""
+        ll = LinkedList[int]()
+        ll.delete(1)
+        assert ll.search(1) is None
+        assert str(ll) == "None"
 
     def test_linked_list_delete_first(self) -> None:
         """Test linked list delete for the first value."""
         ll = LinkedList[int]()
+        ll.insert(1)
         ll.delete(1)
         assert ll.search(1) is None
+        assert str(ll) == "None"
+
+    def test_linked_list_delete_last(self) -> None:
+        """Test linked list delete for the last value."""
+        ll = LinkedList[int]()
+        ll.insert(1)
+        ll.insert(2)
+        ll.delete(2)
+        assert ll.search(2) is None
+        assert str(ll) == "1 -> None"
+
+    def test_linked_list_len(self) -> None:
+        """Test linked list length."""
+        ll = LinkedList[int]()
+        assert len(ll) == 0
+        ll.insert(1)
+        assert len(ll) == 1
+        ll.insert(2)
+        assert len(ll) == 2
+
+    def test_linked_list_iteration(self) -> None:
+        """Test linked list iteration."""
+        ll = LinkedList[int]()
+        ll.insert(1)
+        ll.insert(2)
+        ll.insert(3)
+        assert list(ll) == [3, 2, 1]
+        assert ll.search(2) == 2
